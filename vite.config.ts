f4ee1a843352b977116bdf6e2b5ea56884314ba5
Mathias Lund Ahrn <golvkaldt@gmail.com>
2022-07-29 19:38:47 +0200

    Vite host config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+const repository = "/fem.faq-accordion-card/";
+
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const baseRoute = process.env.NODE_ENV === "production" ? repository : "/";
+  // https://vitejs.dev/config/
+  return defineConfig({
+    base: baseRoute,
+    plugins: [react()],
+  });
+};
